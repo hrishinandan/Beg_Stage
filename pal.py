b4c1320e86b78c6b3d0fe225a4de8e871991dcf5
hrishinandan <hrishinandhan007@gmail.com>
2026-03-10 12:28:25 +0530

    modifed files

--- pal.py
+++ pal.py
@@ -1,10 +1,10 @@
 
 
 
-def is_palindrome(s):
-    reversed_s = s[::-1]
+def is_palindrome(h):
+    reversed_s = h[::-1]
     print(f"Reversed String: {reversed_s}")
-    if s == reversed_s:
+    if h == reversed_s:
         print("The string is a palindrome.")
     else:
         print("The string is not a palindrome.")
